368cfd349a1e51145d7d715240d8e1262d565547
Olha Yuras <olha.yuras@eleks.com>
2023-10-17 14:51:25 +0300

    removed unnecessary rejecct

--- src/functions/math.js
+++ src/functions/math.js
@@ -1,13 +1,11 @@
 export function sum(params) {
-    return new Promise((res, rej) => {
+    return new Promise((res) => {
         res(params.x + params.y);
-        rej(0)
     })
 }
 
 export function multiplicate(params) {
-    return new Promise((res, rej) => {
+    return new Promise((res) => {
         res(params.val * params.multiplier);
-        rej(0)
     })
 }
